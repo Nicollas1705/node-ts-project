7c975f07dd3677c3e999d1ad164263cd5f44edcb
Nicollas1705 <nicollasmota1705@gmail.com>
2025-07-29 13:25:37 -0300

    Create new routes + script + .env

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+// RUN SERVER: npx ts-node-dev ./src/index.ts
+
 import { server } from './server/Server';
 
-server.listen(3333, () => console.log('RUNNING APP!!!'));
+server.listen(process.env.PORT || 3333, () => console.log('RUNNING APP!!!'));
---
+++ src/server/routes/index.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import { StatusCodes } from 'http-status-codes'; // Package to simplify status code
+
+const router = Router();
+
+router.post('/', (req, res) => {
+  console.log(`BODY: ${req.body}`);
+  return res.send(req.body.message);
+});
+
+router.get('/param/:param', (req, res) => {
+  console.log(`PARAM: ${req.params.param}`);
+  return res.send(req.params.param);
+});
+
+router.get('/', (req, res) => {
+  console.log(`QUERY: ${req.query.test}`);
+  return res.send(req.query.test);
+});
+
+router.get('/error', (req, res) => {
+  console.log('Error');
+  // return res.status(404).send();
+  return res.status(StatusCodes.NOT_FOUND).send('Error'); // StatusCodes.NOT_FOUND = 404
+});
+
+export { router };
