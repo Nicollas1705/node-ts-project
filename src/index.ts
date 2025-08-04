67dda28a22c2e86b97da749a15775dc77f6c5017
Nicollas1705 <nicollasmota1705@gmail.com>
2025-08-04 16:26:41 -0300

    Initial tests setup config (jest)

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 // RUN SERVER: npx ts-node-dev ./src/index.ts
-// RUN JS SERVER: node ./build/index.js
 // GENERATE JS: npx tsc
+// RUN JS SERVER: node ./build/index.js
 // If the server start and close with no error message, check if there is already another node instance running behind:
-// lsof -i :PORT (like: lsof -i :3333)
-// If so, kill it with the command: kill PID (like: kill 12345)
+// lsof -i :PORT (like: >lsof -i :3333)
+// If so, kill it with the command: kill PID (like: >kill 12345)
+
+// Libs used for tests: >npm add -D jest ts-jest @types/jest
+// Init tests config: >npm init jest@latest
+// If it asks, install 'create-jest' lib. Check out 'jest.config.ts' file and compare configs
+// 
 
 import { server } from './server/Server';
 
---
+++ tests/jest.setup.ts
@@ -0,0 +1,6 @@
+// * Tests setup (created manually)
+
+import supertest from 'supertest';
+import { server } from '../src/server/Server';
+
+export const testServer = supertest(server);
