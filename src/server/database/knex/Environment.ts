d2bcd7bb5d7c018bae1777a089cbce3b59bec1fd
Nicollas1705 <nicollasmota1705@gmail.com>
2025-08-11 16:08:49 -0300

    Add PostgreSQL to prod env

--- src/server/database/knex/Environment.ts
+++ src/server/database/knex/Environment.ts
@@ -8,7 +8,7 @@ import path from 'path';
 export const development: Knex.Config = { // * Ensure to use the correct variable name 'development'
   client: 'sqlite3',
   useNullAsDefault: true,
-  connection: {
+  connection: { // * The file in where the DB will be
     filename: path.resolve(__dirname, '..', '..', '..', '..', 'database.sqlite') // The file will be created after running
   },
   migrations: {
@@ -26,11 +26,25 @@ export const development: Knex.Config = { // * Ensure to use the correct variabl
   },
 };
 
-export const production: Knex.Config = { // * Ensure to use the correct variable name 'production'
-  ...development,
-};
-
 export const test: Knex.Config = { // * Ensure to use the correct variable name 'test'
   ...development,
-  connection: ':memory:'
+  connection: ':memory:', // * Run tests in memory to not store data
+};
+
+export const production: Knex.Config = { // * Ensure to use the correct variable name 'production'
+  client: 'pg', // PostgreSQL
+  migrations: {
+    directory: path.resolve(__dirname, '..', 'migrations'),
+  },
+  seeds: {
+    directory: path.resolve(__dirname, '..', 'seeds'),
+  },
+  connection: { // * The file in where the DB will be
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT ?? 5432), // * 5432 is the default port of PostgreSQL
+    ssl: { rejectUnauthorized: false },
+  },
 };
--- src/server/database/knex/index.ts
+++ src/server/database/knex/index.ts
@@ -1,5 +1,16 @@
 import knex from 'knex';
 import { development, production, test } from './Environment';
+import 'dotenv/config'; // * Force to initilize the env variables
+import pg from 'pg';
+
+const getDbFromEnv = () => {
+  if (process.env.NODE_ENV === 'prod') { // * Due to PostgreSQL is only used in production
+    // ! On Knex, the PostgreSQL number data is stored as string. To solve it, use 'setTypeParser'
+    pg.types.setTypeParser(20, 'text', parseInt); // * Handle the type INT8 (TypeId = 20), to be parsed from 'text' in 'parseInt' function
+  }
+};
+
+getDbFromEnv(); // * Remember to run it
 
 const getEnv = () => {
   switch (process.env.NODE_ENV) { // NODE_ENV from .env file
